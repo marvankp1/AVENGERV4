c02c7a8320768f22f9965a3fcffdc6d4465b9e5a
MARVAN KP <90860741+marvankp1@users.noreply.github.com>
2021-12-14 07:54:55 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -10,7 +10,7 @@ class Translation(object):
 Just Add Me To Your Group And Channel And Connect Them And See My Pevers 🔥🔥😝
 
 For More Details Click Help Button Below..
-@CrazyBotsz
+@MALAYALAM_MOVES2
 </i>"""    
     
     HELP_TEXT = """
@@ -48,11 +48,11 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/MALAYALAM_MOVES2">MALAYALAM_MOVES2</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
 <b>➥ Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
 
-<b>➥ Source Code</b> : <i><a href="https://github.com/AlbertEinsteinTG/Adv-Auto-Filter-Bot">Click Me</a></i>
+<b>➥ Source Code</b> : <i><a href="https://github.com/marvankp1/AVENGERV4">Click Me</a></i>
 """
