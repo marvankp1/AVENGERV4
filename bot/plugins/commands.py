537047ed8006558417bc805405ffcc574d95f83c
MARVAN KP <90860741+marvankp1@users.noreply.github.com>
2021-12-14 07:59:23 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -35,7 +35,7 @@ async def start(bot, update):
                         [
                             InlineKeyboardButton
                                 (
-                                    'Developers', url="https://t.me/CrazyBotsz"
+                                    '🔰OUR CHANNEL🔰', url="https://t.me/MALAYALAM_MOVES2"
                                 )
                         ]
                     ]
@@ -47,10 +47,10 @@ async def start(bot, update):
         return
 
     buttons = [[
-        InlineKeyboardButton('Developers', url='https://t.me/CrazyBotsz'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('🔰OUR CHANNEL🔰', url='https://t.me/MALAYALAM_MOVES2'),
+        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/marvankp1/AVENGERV4')
     ],[
-        InlineKeyboardButton('Support 🛠', url='https://t.me/CrazyBotszGrp')
+        InlineKeyboardButton('Support 🛠', url='https://t.me/MALAYALAM_MOVES2')
     ],[
         InlineKeyboardButton('Help ⚙', callback_data="help")
     ]]
